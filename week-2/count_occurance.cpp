475fe2a77d61b8a59919352ddf5f5428cd609d69
Chhavi Gupta <gbhawna716@gmail.com>
2026-03-05 14:19:34 +0530

    first and last occurance

---
+++ week-2/count_occurance.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+int firstOccurrence(int arr[], int n, int key){
+    int low = 0, high = n - 1, res = -1;
+    while(low <= high){
+        int mid = (low + high) / 2;
+        if(arr[mid] == key){
+            res = mid;
+            high = mid - 1;
+        }
+        else if(arr[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return res;
+}
+
+int lastOccurrence(int arr[], int n, int key){
+    int low = 0, high = n - 1, res = -1;
+    while(low <= high){
+        int mid = (low + high) / 2;
+        if(arr[mid] == key){
+            res = mid;
+            low = mid + 1;
+        }
+        else if(arr[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return res;
+}
+
+int main(){
+    int T;
+    cin >> T;
+
+    for(int t = 0; t < T; t++){
+        int n;
+        cin >> n;
+
+        int arr[100];
+        for(int i = 0; i < n; i++)
+            cin >> arr[i];
+
+        int key;
+        cin >> key;
+
+        int first = firstOccurrence(arr, n, key);
+
+        if(first == -1)
+            cout << "Key not present" << endl;
+        else{
+            int last = lastOccurrence(arr, n, key);
+            cout << key << " " << last - first + 1 << endl;
+        }
+    }
+
+    return 0;
+}
