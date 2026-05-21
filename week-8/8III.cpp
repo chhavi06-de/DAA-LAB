afc39472be7e9fada337f0a28877257009c9d272
Chhavi Gupta <gbhawna716@gmail.com>
2026-05-21 09:40:40 +0530

    Max MST

---
+++ week-8/8III.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+int maxPrims(vector<vector<pair<int,int>>>& adj, int V, int src){
+
+    priority_queue<pair<int,int>> pq; // MAX HEAP
+
+    vector<int> key(V, INT_MIN);
+    vector<int> parent(V, -1);
+    vector<bool> visited(V, false);
+
+    key[src] = 0;
+    pq.push({0, src});
+
+    int totalCost = 0;
+
+    while(!pq.empty()){
+
+        auto it = pq.top();
+        pq.pop();
+
+        int node = it.second;
+        int weight = it.first;
+
+        if(visited[node]) continue;
+
+        visited[node] = true;
+        totalCost += weight;
+
+        for(auto &ngh : adj[node]){
+
+            int adjNode = ngh.second;
+            int adjWeight = ngh.first;
+
+            if(!visited[adjNode] && adjWeight > key[adjNode]){
+
+                key[adjNode] = adjWeight;
+                parent[adjNode] = node;
+
+                pq.push({key[adjNode], adjNode});
+            }
+        }
+    }
+
+    cout << "\nEdges in Maximum MST:\n";
+
+    for(int i = 0; i < V; i++){
+
+        if(parent[i] != -1){
+
+            cout << parent[i]
+                 << " - "
+                 << i
+                 << " (weight = "
+                 << key[i]
+                 << ")\n";
+        }
+    }
+
+    cout << "\nParent Array:\n";
+
+    for(int i = 0; i < V; i++){
+
+        cout << "Node "
+             << i
+             << " -> Parent: "
+             << parent[i]
+             << "\n";
+    }
+
+    cout << "\nKey Array:\n";
+
+    for(int i = 0; i < V; i++){
+
+        cout << "Node "
+             << i
+             << " -> Key: "
+             << key[i]
+             << "\n";
+    }
+
+    return totalCost;
+}
+
+int main(){
+
+    int V, E;
+    cin >> V >> E;
+
+    vector<vector<pair<int,int>>> adj(V);
+
+    for(int i = 0; i < E; i++){
+
+        int u, v, w;
+        cin >> u >> v >> w;
+
+        adj[u].push_back({w, v});
+        adj[v].push_back({w, u});
+    }
+
+    int cost = maxPrims(adj, V, 0);
+
+    cout << "\nTotal Maximum Cost (MST): "
+         << cost
+         << endl;
+
+    return 0;
+}
